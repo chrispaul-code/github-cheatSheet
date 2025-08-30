1368fe354397b7e34507132f1d94116f93788b36
Christeen Paul <chrispaul1311@gmail.com>
2025-08-30 19:33:25 +0530

    Added above age

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age =21;
-const name ='Christeen Paul'
+const name1 ='Christeen Paul'
 const dob ='13/11/2003'
 
 function sayName(){
-    console.log('Hey ' +name)
+    console.log('Hey ' +name1)
+}
+
+function isAbove18(){
+    return age>=18
 }
